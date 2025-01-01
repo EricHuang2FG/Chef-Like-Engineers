e26d8219a20f967cb71a145c9939ef60252f8ff1
Eric Huang <xilahuade@foxmail.com>
2025-01-01 00:48:41 -0500

    Ah yes liveshare so cool

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 int main() {
+    
+    cout << "test from c--" << endl;
+    // is this even how you print lmao
 
+    // YES I THINK SO
+    // Undefined symbols for architecture x86_64:
+    // WHAT
+    // HELP OIJJOIJSODFJSFJ
+
+    // wait eric the code is on your pc i can't compile
+    // idk how this works lmfao
+    // aidahwdiahwdiawihdisfudofljshdusidiwiedsuohshihiahwidhiwahidhsljicjdifjijaidjiawjdjaiwjdijasjifjjdijijc
+
+    // hold on I ma erroring when compiling too what happened 
+    // whats wrong with int main() lol
+    // I could compile before
+    // :(
+    // help lmfao clang clang clang
+    // i can see your terminal lol
+    // LOLLLLLLLL]
     return 0;
 }
