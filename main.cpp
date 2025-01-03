5700450e19b6851555d6a4c16b1f234f758c5c90
Tait Berlette <54515877+taitberlette@users.noreply.github.com>
2025-01-03 00:31:54 -0500

    Add gitignore for windows nonsense + updated main
    Ignore to hide all the silly windows things 🙈🙈
    
    Stupid main.cpp++ ifdef to change include path for different platforms
    ****NEED TO HAVE WINDOWS DEFINED IN COMPILER COMMAND****
    like clang++ -D WINDOWS

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
+#ifdef WINDOWS
+  #define SDL_MAIN_HANDLED
+  #include <SDL.h>
+  #include <SDL_image.h>
+  #include <SDL_ttf.h>
+  #include <SDL_mixer.h>
+  #include <iostream>
+#else
+  #include <SDL2/SDL.h>
+  #include <SDL2/SDL_image.h>
+  #include <SDL2/SDL_ttf.h>
+  #include <SDL2/SDL_mixer.h>
+#endif 
+
 #include <iostream>
 using namespace std;
 
