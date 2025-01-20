def490e03fa905a1dc6b7e5abf1f689887fc68a6
Eric Huang <xilahuade@foxmail.com>
2025-01-20 11:24:46 -0500

    Increased speed of player but I don't know why walking is funky and weird :(

--- main.cpp
+++ main.cpp
@@ -69,7 +69,7 @@ int main(int argc, char* argv[]) {
     // gameManager.pushState(&b);
 
     int test = 0;
-    Player testPlayer(renderer, 600, 400, 50, 50, 0, 0, 5, 1.5, "./Assets/Player/a");
+    Player testPlayer(renderer, 600, 400, 50, 50, 0, 0, 10, 1.5, "./Assets/Player/a");
 
     while (run) {
         while (SDL_PollEvent(&event)) {
